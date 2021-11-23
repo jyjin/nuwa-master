cafd1b58ec4c9bf8051ce6f5d5ba901a70d9f29b
金建业 <jianye.jin@hand-china.com>
2021-11-23 11:34:20 +0800

    [ADD] 整理目录文件

--- nuwa.config.js
+++ nuwa.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: 4001,
-  name: 'app1',
+  name: 'AppMaster',
 }
--- react/me.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import './index.scss'
-import './index.less'
-const App = () => {
-  return <div>测试子应用环境变量 {DEV}</div>
-}
-
-export default App
